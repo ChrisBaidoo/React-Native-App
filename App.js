530a75833f43a3e98806f0008bfe9cbe0428cffb
christian baidoo <chris_pens@hotmail.co.uk>
2020-06-17 13:36:53 +0100

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  SafeAreaView,
+  Button,
+  Alert,
+  StatusBar,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World</Text>
+
+      <TouchableHighlight
+        onPress={() => {
+          console.log("Hello");
+        }}
+      >
+        <Image
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/id/1/200/300",
+          }}
+        ></Image>
+      </TouchableHighlight>
+
+      <Button
+        title="Click Me"
+        onPress={() => {
+          Alert.alert("Hello There", "Here is what", [
+            { text: "Yes" },
+            { text: "No" },
+          ]);
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
